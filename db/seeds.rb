90d579cf24770e276bfe6a6b56856dc151736e88
andreamaf01 <andrea.maffia3@gmail.com>
2021-09-07 21:37:43 +0200

    added gems, created models and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+
+url = "http://tmdb.lewagon.com/movie/top_rated/"
+
+10.times do |i|
+  # movies = JSON.parse
+
+  # movies.each do |movie|
+  # end
+end
+
+puts "Finished!"
